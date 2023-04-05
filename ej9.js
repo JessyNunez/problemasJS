644159fbf75630425ea6b3dc632266fd31250061
JessyNunez <jessy.nunez0217@gmail.com>
2023-04-04 22:44:11 -0600

    Ejercicio 9, introducir un numero y que nos diga si es + o -

---
+++ ej9.js
@@ -0,0 +1,7 @@
+const num = parseInt(prompt("Introduce un número: "));
+if (num > 0) {
+  console.log("El número es positivo.");
+} else if (num < 0) {
+  console.log("El número es negativo.");
+}
+``
