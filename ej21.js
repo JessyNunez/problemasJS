38f7141008649be3162ef63ce3bd24214ccd5cb3
JessyNunez <jessy.nunez0217@gmail.com>
2023-04-04 23:08:45 -0600

    Ejercicio 21, calcular la suma de los pares e impares del 1 al 1000

---
+++ ej21.js
@@ -0,0 +1,13 @@
+let sumaPares = 0;
+let sumaImpares = 0;
+
+for (let i = 1; i <= 1000; i++) {
+  if (i % 2 === 0) {
+    sumaPares += i;
+  } else {
+    sumaImpares += i;
+  }
+}
+
+alert(`La suma de los números pares entre 1 y 1000 es ${sumaPares}`);
+alert(`La suma de los números impares entre 1 y 1000 es ${sumaImpares}`);
