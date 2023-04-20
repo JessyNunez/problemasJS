28118baa7b993fdd38f42ece013eab94a5bd872f
JessyNunez <jessy.nunez0217@gmail.com>
2023-04-19 21:59:16 -0600

    Ejercicio 32, listado de nombres

---
+++ ej32.js
@@ -0,0 +1,26 @@
+var nombres = [
+  "Jessica",
+  "Melissa",
+  "Pedro",
+  "Graciela",
+  "Xavier",
+  "Javier",
+  "Carlos",
+  "Lucía",
+  "Javier",
+  "Marta",
+  "Pablo",
+  "Elena",
+  "José",
+  "Isabel",
+  "Fernando",
+  "Laura",
+  "Miguel",
+  "Cristina",
+  "Antonio",
+  "Silvia"
+];
+
+for (var i = 0; i < nombres.length; i++) {
+  document.write(nombres[i] + "<br>");
+}
